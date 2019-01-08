3c47d3bed917fce541c3c3e1bb2f196764ce126d
XiaoRGEEK <ylt929@126.com>
2019-01-08 22:33:58 +0800

    test

--- XRBIT.ts
+++ XRBIT.ts
@@ -21,7 +21,7 @@ namespace XRBIT {
         S8 = 0x08
     }
     function i2cwrite(addr: number, reg: number, value: number): void {
-        let buf1 = pins.createBuffer(2);
+        let buf = pins.createBuffer(2);
         buf[0] = reg;
         buf[1] = value;
         pins.i2cWriteBuffer(addr, buf);
@@ -32,13 +32,13 @@ namespace XRBIT {
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
         return val;
     }
-    //% blockId=XRBIT_XiaoRGEEK_SetServoAngle block="XiaoRGEEK_SetServoAngle|Num %Num|Angle %number"
+    //% blockId=XRBIT_XiaoRGEEK_SetServoAngle block="SetServoAngle|Num %Num|Angle %Angle"
     //% weight=94
     //% blockGap=10
-    //% color="#006400"
+    //% color="#0fbc11"
     //% Num.min=1 Num.max=8 Angle.min=0 Angle.max=180
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
-    export function XiaoRGEEK_SetServoAngle(Num: SER_NUM, Angle: number): void {
+    export function SetServoAngle(Num: SER_NUM, Angle: number): void {
         let buf1 = pins.createBuffer(2);
         let buf2 = pins.createBuffer(2);
         buf1[0] = 0xFF;
