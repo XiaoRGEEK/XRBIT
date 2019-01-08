ad807e52d43e535842ed5609778fb1971f7b2fd4
XiaoRGEEK <ylt929@126.com>
2019-01-08 22:43:12 +0800

    test

--- XRBIT.ts
+++ XRBIT.ts
@@ -32,7 +32,7 @@ namespace XRBIT {
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
         return val;
     }
-    //% block="SetServoAngle|Num %Num|Angle %Angle"
+    //% blockId=XRBIT_SetServoAngle block="SetServoAngle|Num %Num|Angle %Angle"
     //% weight=94
     //% blockGap=10
     //% color="#0fbc11"
