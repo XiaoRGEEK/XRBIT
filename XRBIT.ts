7cf5474aee05dcc9deb1b596b2adec3ce49cf395
XiaoRGEEK <ylt929@126.com>
2019-01-10 18:42:33 +0800

    test

--- XRBIT.ts
+++ XRBIT.ts
@@ -7,7 +7,7 @@
  * 自定义图形块
  */
 
-//% weight=5 color=#9900CC icon="\uf53b"
+//% weight=5 color=#9900CC icon="\uf1b9"
 namespace XRBIT {
     const XRBIT_ADDRESS = 0x17
     export enum SER_NUM {
@@ -19,6 +19,18 @@ namespace XRBIT {
         S6 = 0x06,
         S7 = 0x07,
         S8 = 0x08
+    }
+    export enum MovementGroups {
+        //% blockId="goforward" block="goforward"
+        goforward = 0x01,
+        //% blockId="goback" block="goback"
+        goback = 0x02,
+        //% blockId="turnleft" block="turnleft"
+        turnleft = 0x03,
+        //% blockId="turnright" block="turnright"
+        turnright = 0x04,
+        //% blockId="stop" block="stop"
+        stop = 0x00
     }
     function i2cwrite(addr: number, reg: number, value: number): void {
         let buf = pins.createBuffer(2);
@@ -50,6 +62,20 @@ namespace XRBIT {
         buf1[0] = 0xFF;
         buf1[1] = Num;
         buf2[0] = Angle;
+        buf2[1] = 0xFF;
+        pins.i2cWriteBuffer(XRBIT_ADDRESS,buf1);
+        pins.i2cWriteBuffer(XRBIT_ADDRESS,buf2);
+    }
+    //% blockId=XRBIT_SetServoBot block="SetServoBot|%direction"
+    //% weight=94
+    //% blockGap=10
+    //% color="#0fbc11"
+    export function SetServoBot(direction: MovementGroups): void {
+        let buf1 = pins.createBuffer(2);
+        let buf2 = pins.createBuffer(2);
+        buf1[0] = 0xFF;
+        buf1[1] = 0x12;
+        buf2[0] = direction;
         buf2[1] = 0xFF;
         pins.i2cWriteBuffer(XRBIT_ADDRESS,buf1);
         pins.i2cWriteBuffer(XRBIT_ADDRESS,buf2);
