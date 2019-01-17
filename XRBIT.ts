8aa385f62b869be8ae66cf2fc73defff068efc0a
XiaoRGEEK <ylt929@126.com>
2019-01-17 17:29:48 +0800

    test

--- XRBIT.ts
+++ XRBIT.ts
@@ -173,4 +173,45 @@ namespace XRBIT {
         pins.i2cWriteBuffer(XRBIT_ADDRESS,buf2);
     }
 
+    //% blockId=XR_IRtest
+    //% weight=94
+    //% blockGap=10
+    //% color="#0fbc11"
+    export function XR_IRtest(body: () => void):void {
+        let buf = pins.createBuffer(4);
+        let startcont = 10;
+        let i = 0;
+        let j = 0;
+        while (startcont--) { 
+            control.waitMicros(793);
+            if (pins.digitalReadPin(DigitalPin.P1) == 1) { 
+                return;
+            }
+        }
+
+        while (pins.digitalReadPin(DigitalPin.P1) == 0);
+
+        control.waitMicros(2305);
+
+        if (pins.digitalReadPin(DigitalPin.P1) == 0) { 
+            return;
+        }
+
+        while (pins.digitalReadPin(DigitalPin.P1) == 1);
+
+        for (i = 0; i < 4; i++) { 
+            for (j = 0; j < 8; j++) { 
+                while (pins.digitalReadPin(DigitalPin.P1) == 0);
+                buf[i] >>= 1;
+                control.waitMicros(793);
+                if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                    buf[i] |= 0x80;
+                    while (pins.digitalReadPin(DigitalPin.P1) == 1);
+                }
+            }
+        }
+
+        serial.writeNumber(buf[2]);
+    }
+
 }
