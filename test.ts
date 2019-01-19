3bc2d0e2343b13990a1d2faf62a5970a3144d8d6
XiaoRGEEK <ylt929@126.com>
2019-01-19 12:57:33 +0800

    irremote

--- test.ts
+++ test.ts
@@ -1,14 +1,2 @@
 /* tests go here; this will not be compiled when this package is used as a library*/
-pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
-let dat = 9;
-let temp = 9;
-basic.forever(function () {
 
-    temp = XRBIT.XR_IRremote();
-    /*if (dat != temp) { 
-        dat = temp;
-        serial.writeNumber(dat);
-
-    }*/
-    //serial.writeNumber(XRBIT.XR_IRremote())
-})
